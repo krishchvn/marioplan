c727bb63f870fe6c03e556c89922292b24b42753
skukreja <skukreja434@gmail.com>
2020-08-13 18:10:18 +0530

    getStateErrorRemoved

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
-import rootReducers from './store/reducers/rootReducers'
-import thunk from 'redux-thunk'
-import { reduxFirestore , getFirestore } from 'redux-firestore'
-import { reactReduxFirebase , getFirebase } from 'react-redux-firebase'
-import fbConfig from './components/config/fbConfig'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import rootReducers from "./store/reducers/rootReducers";
+import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from "./components/config/fbConfig";
 
-const store = () => createStore(rootReducers , 
+const store = createStore(
+  rootReducers,
   compose(
-    applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore}))),
-  reduxFirestore(fbConfig),
-  reactReduxFirebase(fbConfig)
-  );
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store = {store }>
-    <App /> 
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,17 +1,22 @@
 export const createProject = (project) => {
-    return (dispatch, getState , {getFirebase, getFirestore}) => {
-        //async call to database
-        const firestore = getFirestore();
-        firestore.collection('projects').add({
-            ...project,
-            authorFirstName: 'Ryu',
-            authorLastName: 'Hoshi',
-            authorId: 2324,
-            createdAt: new Date()
-        }).then(() => {
-            dispatch({type: 'CREATE_PROJECT' , project });
-        }).catch((err) => {
-            dispatch({type: 'CREATE_PROJECT_ERROR' , err });
-        })
-    }
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
+    firestore
+      .collection("projects")
+      .add({
+        ...project,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: authorId,
+        createdAt: new Date(),
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT_SUCCESS" });
+      })
+      .catch((err) => {
+        dispatch({ type: "CREATE_PROJECT_ERROR" }, err);
+      });
+  };
 };
